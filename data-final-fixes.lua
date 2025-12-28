0882c10d70749b2f4a2bb6d19f32746b66bc6663
Talandar <Talandar66@protonmail.com>
2025-12-28 23:38:29 +0100

    quality changes

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -83,15 +83,27 @@ local function hide_by_name(name)
 		end
 	end
 end
-
+-- assembling machines changes
 hide_technology_and_rewire("automation", "automation-science-pack")
-hide_technology_and_rewire("electric-mining-drill", "diesel-mining-drill")
 hide_technology_and_rewire("automation-3", "production-science-pack")
-
 data.raw["assembling-machine"]["steam-assembling-machine"].next_upgrade = "assembling-machine-2"
 data.raw["assembling-machine"]["assembling-machine-2"].next_upgrade = "diesel-assembling-machine"
 data.raw["assembling-machine"]["diesel-assembling-machine"].next_upgrade = nil
-
 hide_by_name("assembling-machine-1")
 hide_by_name("assembling-machine-3")
+table.insert(
+	data.raw["recipe"]["assembling-machine-2"].ingredients,
+	{ type = "item", name = "iron-plate", amount = 15 }
+)
+-- mining drill changes
+hide_technology_and_rewire("electric-mining-drill", "diesel-mining-drill")
 hide_by_name("electric-mining-drill")
+
+-- quality changes
+hide_technology_and_rewire("epic-quality", "quality-module")
+hide_technology_and_rewire("legendary-quality", "quality-module")
+for _, furnace in pairs(data.raw["mining-drill"]) do
+	furnace.allowed_effects = { "consumption", "speed", "productivity", "pollution" }
+end
+data.raw["assembing-machine"]["crusher"].allowed_effects = { "consumption", "speed", "productivity", "pollution" }
+data.raw["furnace"]["recycler"].allowed_effects = { "consumption", "speed", "productivity", "pollution" }
