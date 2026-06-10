d2501d9ea0e22ef138e067882c84983a08a3b6bd
Talandar <talandar66@protonmail.com>
2026-06-10 09:09:48 +0200

    softlock with stone fix

--- data-updates.lua
+++ data-updates.lua
@@ -1,2 +1,27 @@
-
 --table.insert(data.raw["technology"]["space-platform-thruster"].prerequisites, "advanced-asteroid-processing")
+data:extend({
+	{
+		type = "recipe",
+		name = "stone-furnace-from-bricks",
+		enabled = true,
+		energy_required = 0.5,
+		icons = {
+			{
+				icon = data.raw.item["stone-furnace"].icon,
+				icon_size = data.raw.item["stone-furnace"].icon_size or 64,
+			},
+			{
+				icon = data.raw.item["stone-brick"].icon,
+				icon_size = data.raw.item["stone-brick"].icon_size or 64,
+				scale = 0.25,
+				shift = { 8, 8 },
+			},
+		},
+		ingredients = {
+			{ type = "item", name = "stone-brick", amount = 3 },
+		},
+		results = {
+			{ type = "item", name = "stone-furnace", amount = 1 },
+		},
+	},
+})
